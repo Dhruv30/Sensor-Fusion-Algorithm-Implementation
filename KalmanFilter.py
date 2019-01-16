6fbaa80bdf93f32fbf70d67347fda50fa1bcfdfc
Dhruv30 <dhruvmanani2000@gmail.com>
2019-01-15 19:23:13 -0500

    Added Comments

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -1,13 +1,17 @@
 import numpy as np
 
+# Actual data of the object, each term represents the data at 1 arbitrary unit of time (Ex. 1 minute)
 positionData = np.array([4000, 4260, 4550, 4860, 5110])
 velocityData = np.array([280, 282, 285, 286, 290])
 
+# The time interval between each of the terms in the above arrays (set to 1 for simplicity)
 time = 1;
 
+# Represents the initial error level designated to the position and velocity (used for the calculation of the initial covariance matrix)
 positionErrorInit = 20
 velocityErrorInit = 5
 
+# Represents the actual error level designated to the position and velocity (used for calculating later covariance matrices)
 positionErrorActual = 21
 velocityErrorActual = 6
 
@@ -18,6 +22,7 @@ def statePrediction(position, velocity, time):
     state = np.array([[position],
                              [velocity]])
     
+    # Predicts the next state of the object based on its velocity and position
     newState = np.matmul(dynamicMatrix, state)
     
     return newState
